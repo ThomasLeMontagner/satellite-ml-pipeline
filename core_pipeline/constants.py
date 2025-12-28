c4f48268ab80f003c5ea249f2512686cc00eec1a
ThomasLeMontagner <thomas.le-montagner@hotmail.fr>
2025-12-28 17:30:49 +0100

    added type annotation

--- core_pipeline/constants.py
+++ core_pipeline/constants.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 DATA_DIRECTORY = Path(__file__).resolve().parent.parent / "data"
 RAW_DATA_DIRECTORY: Path = DATA_DIRECTORY / "raw"
-TILES_DIRECTORY = DATA_DIRECTORY / "tiles"
+TILES_DIRECTORY: Path = DATA_DIRECTORY / "tiles"
