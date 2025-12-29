cddddb6c838033539e4ed261a7f6399e122a717f
ThomasLeMontagner <thomas.le-montagner@hotmail.fr>
2025-12-29 13:59:05 +0100

    fixed models directory

--- constants.py
+++ constants.py
@@ -5,4 +5,4 @@ from pathlib import Path
 DATA_DIRECTORY = Path(__file__).resolve().parent / "data"
 RAW_DATA_DIRECTORY: Path = DATA_DIRECTORY / "raw"
 TILES_DIRECTORY: Path = DATA_DIRECTORY / "tiles"
-MODELS_DIRECTORY: Path = DATA_DIRECTORY / "model" / "models"
+MODELS_DIRECTORY: Path = Path(__file__).resolve().parent / "model" / "models"
